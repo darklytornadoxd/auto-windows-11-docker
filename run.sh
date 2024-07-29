553587c9cb50566e02aa8f82fcfbbe73169972c0
DarklyTornado (Zanny/ZannyTornado) <160201858+darklytornadoxd@users.noreply.github.com>
2024-07-29 14:43:53 -0700

    Create run.sh #1

---
+++ run.sh
@@ -0,0 +1,36 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+mkdir windows
+cd mkdir
+cat << EOF > compose.yaml
+services:
+  windows:
+    image: dockurr/windows
+    container_name: windows
+    environment:
+      RAM_SIZE: "6G"
+      CPU_CORES: "2"
+      VERSION: "win11"
+    devices:
+      - /dev/kvm
+    cap_add:
+      - NET_ADMIN
+    ports:
+      - 8006:8006
+      - 3389:3389/tcp
+      - 3389:3389/udp
+    stop_grace_period: 2m
+EOF
+docker compose up -d
